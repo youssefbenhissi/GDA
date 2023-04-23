fbba756b5db412272b88194a442f8dd80db902a3
Youssef Benhissi <youssef.benhissi@quodfinancial.com>
2023-04-23 15:46:52 +0200

    improvement in splash screen

--- lib/splash_screen_bloc/splash_screen_widget.dart
+++ lib/splash_screen_bloc/splash_screen_widget.dart
@@ -6,11 +6,29 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image.asset(
-          'assets/images/Logo.jpg',
-          fit: BoxFit.cover,
-        ),
+      body: Column(
+        children: [
+          Image.asset(
+            'assets/images/Logo.jpg',
+            fit: BoxFit.cover,
+          ),
+          const Text(
+            'MINISTRE DE L\'AGRICULTURE',
+            textAlign: TextAlign.center,
+          ),
+          const Text(
+            'DES RESSOURCES HYDRAULIQUES ET DE LA PECHE MARITIME',
+            textAlign: TextAlign.center,
+          ),
+          const Text(
+            'Système d\'Information pour le suivi et l\'évaluation de\nl\'expolitation et de la gestion des Systèmes\nd\'Alimentation de l\'Eau Potable (SAEP) en milieu rural',
+            textAlign: TextAlign.center,
+          ),
+          const Text(
+            'منظومة التصرف في شبكات الماء الصالح للشرب \nلدى مجامع التنمية الفلاحية',
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
