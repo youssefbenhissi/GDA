4f81843a94a8a2abf4948e4a208de4c1888cdd6e
DESKTOP-040L16N\FE <mohamed-ali.nammouchi@quodfinancial.com>
2023-04-24 15:38:41 +0200

    improve consultation screen

--- lib/consultation_screen_bloc/consultation_screen_widget.dart
+++ lib/consultation_screen_bloc/consultation_screen_widget.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:pfe_iheb/statistics_bloc/statistic_card_widget.dart';
 import 'package:pfe_iheb/utils/app_colors.dart';
 import 'package:pfe_iheb/utils/drawer.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+part 'gda_popup.dart';
 
 class ConsultationPage extends StatelessWidget {
   const ConsultationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    String dropdownValue = 'Dog';
     return Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: AppColors.primaryblue,
@@ -17,8 +18,83 @@ class ConsultationPage extends StatelessWidget {
             title: Text(
           AppLocalizations.of(context)!.consultationTitre,
         )),
-        body: const Center(
-          child: Text("youssef"),
+        body: Column(
+          children: [
+            ColoredBox(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0, right: 8.0, left: 8.0),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        AppLocalizations.of(context)!.moisTitre,
+                        style: const TextStyle(color: Colors.lightBlue),
+                      ),
+                    ),
+                    DropdownButton<String>(
+                      isExpanded: true,
+                      value: dropdownValue,
+                      items: <String>['Dog', 'Cat', 'Tiger', 'Lion']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {},
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        AppLocalizations.of(context)!.anneeTitre,
+                        style: const TextStyle(color: Colors.lightBlue),
+                      ),
+                    ),
+                    DropdownButton<String>(
+                      isExpanded: true,
+                      value: dropdownValue,
+                      items: <String>['Dog', 'Cat', 'Tiger', 'Lion']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {},
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        AppLocalizations.of(context)!.gdaTitre,
+                        style: const TextStyle(color: Colors.lightBlue),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) =>
+                              _buildPopupDialog(context),
+                        );
+                      },
+                      child: DropdownButton<String>(
+                        isExpanded: true,
+                        items: const [],
+                        iconDisabledColor: Colors.black,
+                        onChanged: (String? newValue) {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ));
   }
 }
---
+++ lib/consultation_screen_bloc/gda_popup.dart
@@ -0,0 +1,22 @@
+part of 'consultation_screen_widget.dart';
+
+Widget _buildPopupDialog(BuildContext context) {
+  return AlertDialog(
+    title: const Text('Popup example'),
+    content: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const <Widget>[
+        Text("Hello"),
+      ],
+    ),
+    actions: <Widget>[
+      TextButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        child: const Text('Close'),
+      ),
+    ],
+  );
+}
