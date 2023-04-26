fe17f97b678948de4ef9231ce373a7b12d2bcd05
DESKTOP-040L16N\FE <mohamed-ali.nammouchi@quodfinancial.com>
2023-04-26 14:35:09 +0200

    fiche gda

--- lib/fiche_gda_bloc/fiche_gda_widget.dart
+++ lib/fiche_gda_bloc/fiche_gda_widget.dart
@@ -13,10 +13,97 @@ class FicheGDAPage extends StatelessWidget {
       backgroundColor: AppColors.primaryblue,
       drawer: const ApplicationDrawer(),
       appBar: AppBar(
-          title: Text(
-        AppLocalizations.of(context)!.donnesIdentificationTitre,
-      )),
-      body: const Text("fiche GDA"),
+        title: Text(
+          AppLocalizations.of(context)!.donnesIdentificationTitre,
+        ),
+        actions: <Widget>[
+          Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: const Icon(
+                  Icons.arrow_right_alt,
+                  size: 26.0,
+                ),
+              )),
+        ],
+      ),
+      body: ColoredBox(
+        color: Colors.white,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0, left: 8.0, top: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    "Gouvernorat",
+                    style: TextStyle(color: Colors.lightBlue),
+                  ),
+                  Text("41"),
+                  Text("KAIROUAN"),
+                ],
+              ),
+            ),
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    "Délégation",
+                    style: TextStyle(color: Colors.lightBlue),
+                  ),
+                  Text("4145"),
+                  Text("Sbikha"),
+                ],
+              ),
+            ),
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    "GDA",
+                    style: TextStyle(color: Colors.lightBlue),
+                  ),
+                  Text("4145"),
+                  Text("KAIROUAN"),
+                ],
+              ),
+            ),
+            const Divider(),
+            const SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              "assets/images/goutte.png",
+            ),
+            const Divider(),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 8.0, left: 8.0),
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                  onPressed: () {},
+                  child: Text(AppLocalizations.of(context)!.detailsGDATitre),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
